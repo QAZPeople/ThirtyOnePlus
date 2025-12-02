5921a9f1a384cc938f9230b2933ed59839cb089d
QAZPeople <takep_t_1508@yahoo.co.jp>
2025-12-03 00:51:29 +0900

    Add files via upload

---
+++ qmk_firmware/keyboards/thirtyoneplus/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xc4, 0xe2, 0x3d, 0x07, 0x9b, 0x58, 0x78, 0x2d}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
